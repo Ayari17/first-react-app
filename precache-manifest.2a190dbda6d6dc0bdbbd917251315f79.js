a3ef21a00ddd0c2d67b985f2d25ddaac5515322b
Ayari Ahmed <ayari17@hotmail.fr>
2020-04-01 00:56:50 +0100

    Updates

---
+++ precache-manifest.2a190dbda6d6dc0bdbbd917251315f79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981c5dfe504af31fdde5a11368aaef3e",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "324cc4194a0a1e259255",
+    "url": "/first-react-app/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "606eb10280648fbaf447",
+    "url": "/first-react-app/static/css/main.f23cc4f8.chunk.css"
+  },
+  {
+    "revision": "324cc4194a0a1e259255",
+    "url": "/first-react-app/static/js/2.58fc17f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.58fc17f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606eb10280648fbaf447",
+    "url": "/first-react-app/static/js/main.886f7549.chunk.js"
+  },
+  {
+    "revision": "540cc87087476b398774",
+    "url": "/first-react-app/static/js/runtime-main.2e926a30.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/first-react-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
